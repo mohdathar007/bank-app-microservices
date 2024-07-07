d1413d55bfdc78ad918c4a8aafdbaaaa05186f57
Athar <mohdathar8888@gmail.com>
2024-07-07 18:15:03 +0530

    account microservice crud opration completed

---
+++ accounts/src/main/java/com/athar/accounts/repository/CustomerRepository.java
@@ -0,0 +1,17 @@
+package com.athar.accounts.repository;
+
+
+
+import com.athar.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
